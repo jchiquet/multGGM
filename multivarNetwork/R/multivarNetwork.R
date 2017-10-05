70a0c61a908339f53de5ba1aefdb842e43df5c94
Julien Chiquet <julien.chiquet@gmail.com>
2017-10-05 08:06:51 +0200

    started a simple package to simplfy use of multivariate function

---
+++ multivarNetwork/R/multivarNetwork.R
@@ -0,0 +1,104 @@
+#' @title Fit a collection of networks from multivariate experiments
+#'
+#' @description The inference procedure is multivariate neighborhood selection with group Lasso penalty.
+#' If an univariate experiment is provided, the usual neighborhood selection with lasso penalty applies.
+#'
+#' @param X a list of matrices with the same dimension, corresponding to multiple experiments related
+#' to the same individuals and variables, e.g., proteomics and transcriptomics data.
+#' @param sym.rule a character, either "AND" or "OR" to define the  post-symmetrization rule applied to the coefficients in neighborhood selection
+#' @param cv.choice a character for defining the cross-validation rule used to choose the penalty level (i.e., the number of edges in the the network).
+#' If "none", the whole regulariazation paths is sent back. If "min" or "1se", penalties are cross-validation for each variable
+#' with the corresponding rule.
+#' @param nlambda integer defining the number of penalty levels used on the grid
+#' @param min.ratio the grid of penalties starts by lambda.max, that is,  minimal penalty level for selecting no edge in the network, then decrease on a
+#' log scale until min.ratio* lambda.max.
+#' @param mc.cores for distributing the computation over the variables. Default is 1 (no parallelization).
+#'
+#' @importFrom parallel mclapply
+#' @importFrom pbmcapply pbmclapply
+#' @import glmnet
+#' @import gglasso
+#' @export
+multivarNetwork <- function(X, sym.rule="AND", cv.choice=c("none", "1se", "min"),
+                            nlambda=50, min.ratio=1e-3, mc.cores=1) {
+
+  cv.choice <- match.arg(cv.choice)
+  LAPPLY <- ifelse(mc.cores > 1, pbmclapply, mclapply)
+
+  stopifnot(is.list(X) | is.matrix(X))
+  if (is.matrix(X)) X <- list(X)
+
+  ## scaling to make everything comparable
+  X <- lapply(X, scale)
+
+  ## problem dimension
+  K <- length(X)
+  p <- ncol(X[[1]])
+  n <- nrow(X[[1]])
+
+  ## define group indexes
+  group <- rep(rep(1:(p-1),K), K)
+
+  ## create the list of data sets corresponding to each variable
+  training_sets <- lapply(1:p, function(j) {
+    y <- Reduce("cbind", lapply(X, function(x) x[, j]))
+    x <- Reduce("cbind", lapply(X, function(x) x[,-j]))
+    list(
+      Y = as.vector(y),
+      X = bdiag(rep(list(x), K)),
+      j = j
+    )
+  })
+
+  ## Get the largest lambda value across all variable that produce a null vector as estimate
+  lambda.max <- max(sapply(training_sets, function(data) {
+    return(max(sqrt(rowsum(as.numeric(crossprod(data$X, data$Y)^2), group)/(2*n))))
+  }))
+
+  lambda <- 10^seq(from=log10(lambda.max),log10(min.ratio*lambda.max),len=nlambda)
+
+  ## compute all coefficients
+  f_infer <- ifelse(K==1, lasso, grplasso)
+
+  coefficients <- do.call(rbind,LAPPLY(training_sets, function(d) {
+    coef <- f_infer(d$X, d$Y, group, lambda, cv.choice)
+    beta  <- matrix(0, p, ncol(coef))
+    beta[-d$j, ] <- as.matrix(coef)
+    beta
+  }, mc.cores=mc.cores))
+
+  networks <- apply(coefficients, 2, function(beta) {
+    net <- matrix(beta != 0,p,p)
+    net <- Matrix(switch(sym.rule,
+                         "AND" = pmin(net,t(net)),
+                         "OR"  = pmax(net,t(net))))
+    net
+  })
+
+  return(list(coefficients = coefficients, networks = networks, penalties = lambda))
+}
+
+lasso <- function(x, y, group, lambda, cv.choice) {
+  if (cv.choice == "none") {
+    out <- glmnet(x, y, lambda=lambda, intercept=FALSE, standardize=FALSE)
+    beta <- coef.glmnet(out)[-1,]
+  } else {
+    out <- cv.glmnet(x, y, lambda=lambda, intercept=FALSE, standardize=FALSE)
+    beta <- matrix(coef.cv.glmnet(out, s=paste("lambda",cv.choice, sep="."))[-1], ncol=1)
+  }
+  abs(beta)
+}
+
+grplasso <- function(x, y, group, lambda, cv.choice) {
+  o <- order(group)
+  x <- as.matrix(x)[, o]
+  if (cv.choice == "none") {
+    out <- gglasso(x, y, group[o], lambda=lambda, intercept=FALSE)
+    beta <- coef(out)[-1,]
+  } else {
+    out <- cv.gglasso(x, y, group[o], lambda=lambda, intercept=FALSE)
+    beta <- matrix(coef(out, s=paste("lambda",cv.choice, sep="."))[-1], ncol=1)
+  }
+  beta <- apply(beta[order(o), , drop=FALSE], 2, function(b) sqrt(rowsum(b^2,group)))
+}
+
