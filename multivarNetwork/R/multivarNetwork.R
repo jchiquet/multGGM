d1b4f06ef4170d77c2717b780ae773e9595d2f74
Julien Chiquet <julien.chiquet@gmail.com>
2019-02-05 09:04:00 +0100

    typos in multivarNetwork preventing package build

--- multivarNetwork/R/multivarNetwork.R
+++ multivarNetwork/R/multivarNetwork.R
@@ -21,7 +21,7 @@
 #' @import gglasso
 #' @import stabsel
 #' @export
-multivarNetwork <- function(X, sym.rule="AND", select=c("none", "1se", "min" ,git"stabsel"),
+multivarNetwork <- function(X, sym.rule="AND", select=c("none", "1se", "min" ,"stabsel"),
                             nlambda=50, min.ratio=1e-3, mc.cores=1, nfold=5, cutoff=0.75) {
 
   select <- match.arg(select)
